732e5b865fa00b7d1c059e9f404efe17ec15058e
Dmitry Eskin <dmitry.eskin@onixs.biz>
2017-11-26 18:42:11 +0300

    dynamically genetae run script

--- run_project.sh
+++ run_project.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+
+genRunScript()
+{
+
+    UID=`stat -c "%u" ~`
+    GID=`stat -c "%g" ~`
+
+    mkdir $scriptPath
+
+    cat > $scriptPath/$scriptName <<-EOF
+		#!/bin/sh
+
+		rm -rf /prg/build/devel/*
+
+		chown -R $UID:$GID /root /prg /work /script
+		echo ":x:""$UID":"$GID""::/root:/bin/bash" >> /etc/passwd
+		sudo -u \#"$UID" scl enable onixs-devtoolset "/work/bin/clion.sh /prg/CMakeLists.txt"
+		EOF
+
+    chmod +x $scriptPath/$scriptName
+}
+
+
 prgName=$1
 os=centos/7
 
@@ -13,12 +36,13 @@ prgPath=/home/dmitry/Data/work/$prgName
 outputDir=/tmp/DevOutput
 
 outputPath=$outputDir/$prgName
-
-
 clionPath=/home/dmitry/Data/work/Clion
 
+scriptPath=/tmp/Script
+scriptName=run_on_docker.sh
 
 [ -f $outputPath ] && mkdir $outputPath
+[ -f $scriptPath/$scriptName ] && genRunScript
 
 
 echo "os:" $os
@@ -26,25 +50,19 @@ echo "projet path:" $prgPath
 echo "projet output:" $outputPath
 echo ""
 
-
 xhost +local:
 
 
-uid=`stat -c "%u" ~`
-gid=`stat -c "%g" ~`
-
 docker run --rm -it                                                                                 \
 -e DISPLAY=unix$DISPLAY                                                                             \
--e UID=$uid                                                                                         \
--e GID=$gid                                                                                         \
 --security-opt seccomp=unconfined                                                                   \
 -v /tmp/.X11-unix:/tmp/.X11-unix                                                                    \
 -v $clionPath/clion-2017.2.3:/work                                                                  \
 -v $clionPath/SharedConfig/CLion2017.2:/root/.CLion2017.2                                           \
 -v $clionPath/SharedConfig/userPrefs:/root/.java/.userPrefs/jetbrains                               \
--v $clionPath:/script                                                                               \
+-v $scriptPath:/script                                                                              \
                                                                                                     \
 -v $prgPath:/prg                                                                                    \
 -v $outputPath:/prg/build/devel                                                                     \
 onixs-docker-images.jfrog.io/$os/devel/ui/$envName                                                  \
-/script/run_on_docker.sh
+/script/$scriptName
