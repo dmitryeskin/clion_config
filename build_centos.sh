d77f040d4e85c62804c9634127405598b1eae6a1
Dmitry Eskin <dmitry.eskin@onixs.biz>
2017-12-02 23:22:32 +0300

    valgrind integration enabled

--- build_centos.sh
+++ build_centos.sh
@@ -7,7 +7,7 @@ docker pull onixs-docker-images.jfrog.io/$os/devel
 cat << EOF > Dockerfile
 FROM onixs-docker-images.jfrog.io/$os/devel 
 RUN yum update
-RUN yum install -y libXtst.x86_64
+RUN yum install -y libXtst.x86_64 valgrind
 ADD ./fonts/UbuntuMono-BI.ttf /usr/share/fonts/UbuntuMono-BI.ttf
 ADD ./fonts/UbuntuMono-B.ttf /usr/share/fonts/UbuntuMono-B.ttf
 ADD ./fonts/UbuntuMono-RI.ttf /usr/share/fonts/UbuntuMono-RI.ttf
