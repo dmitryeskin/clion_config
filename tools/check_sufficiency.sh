6d23018331f1d8467bdb5ba77ead8e08e6162e1c
Dmitry Eskin <dmitry.eskin@onixs.biz>
2017-12-08 22:58:30 +0300

    do not create temporary files

--- tools/check_sufficiency.sh
+++ tools/check_sufficiency.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
 
-
 checkedDir=/prg/include/
-
-oldpath=`pwd`
 cd $checkedDir
-
 
 for f in $(find * -name '*.h'); 
 do 
 echo $f
-cat << EOF > test.cpp
-#include <$f>
-int main(){return 0;}
-EOF
-g++ test.cpp -o test.o -I$checkedDir -I/prg/build/devel/debug/include 2>&1 | grep -ve 'test.cpp'
-rm -rf test.*
-
+echo "#include <$f>" | g++ -x c++ -std=c++03 -fsyntax-only -I$checkedDir -I/prg/build/devel/debug/include - 2>&1 | grep -ve '<stdin>'
 done
 
 
-cd $oldpath
