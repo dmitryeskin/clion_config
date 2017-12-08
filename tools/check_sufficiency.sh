e97fb371585ba333bfe667c5c1ec47e08689de39
Dmitry Eskin <dmitry.eskin@onixs.biz>
2017-12-09 00:09:39 +0300

    hilight

--- tools/check_sufficiency.sh
+++ tools/check_sufficiency.sh
@@ -3,10 +3,19 @@
 checkedDir=/prg/include/
 cd $checkedDir
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 for f in $(find * -name '*.h'); 
 do 
-echo $f
-echo "#include <$f>" | g++ -x c++ -std=c++03 -fsyntax-only -I$checkedDir -I/prg/build/devel/debug/include - 2>&1 | grep -ve '<stdin>'
+echo -e $GREEN $f $NC
+
+echo "#include <$f>" |                                                                              \
+g++ -x c++ -std=c++03 -fsyntax-only -I$checkedDir -I/prg/build/devel/debug/include - 2>&1 |         \
+grep -ve '<stdin>' |                                                                                \
+sed -e 's/\(.*error.*\)/\o033[31m\1\o033[0m/'
+
 done
 
 
