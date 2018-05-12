9fa66ee5f0e5f5185d7324e38501a2ea5d0648ff
Dmitry Eskin <dmitry.eskin@onixs.biz>
2018-05-13 00:23:26 +0300

    castom dts automatic support

--- projects/run_fw24x7.sh
+++ projects/run_fw24x7.sh
@@ -5,4 +5,4 @@ prgName=fw24x7
 
 echo "Opening "$prgName
 
-../run_project.sh $prgName $prgName /dts/4
+../run_project.sh $prgName $prgName centos/7/dts/4
--- run_project.sh
+++ run_project.sh
@@ -1,12 +1,34 @@
 #!/bin/sh
 
+prgName=$1
+
+
+if [ -z "$3" ]; then
+    echo "using default os"
+    os="centos/7"
+else
+    os=$3
+fi
+
+
+optional_dts=''
+if echo $os | grep -q "/dts/4"; then
+    optional_dts='devtoolset-4'
+elif echo $os | grep -q "/dts/2"; then
+    optional_dts='devtoolset-2'
+elif echo $os | grep -q "/dts/"; then
+    echo "!Unknown dts set up: "$os
+    exit 1;
+fi
+
+
 
 genRunScript()
 {
     UID=`stat -c "%u" ~`
     GID=`stat -c "%g" ~`
 
-    mkdir $scriptPath
+    mkdir -p $scriptPath
 
     cat > $scriptPath/$scriptName <<-EOF
 		#!/bin/sh
@@ -15,24 +37,12 @@ genRunScript()
 
 		chown -R $UID:$GID /root /prg /work /script /tools
 		echo ":x:$UID:$GID::/root:/bin/bash" >> /etc/passwd
-		sudo -u \#$UID scl enable onixs-devtoolset "/work/bin/clion.sh /prg/CMakeLists.txt"
+		sudo -u \#$UID scl enable $optional_dts onixs-devtoolset "/work/bin/clion.sh /prg/CMakeLists.txt"
 		EOF
 
     chmod +x $scriptPath/$scriptName
 }
 
-
-prgName=$1
-os=centos/7
-
-
-if [ -z "$3" ]; then
-    echo "using default dts"
-    dts=""
-else
-    echo "using dts "$3
-    dts=$3
-fi
 
 
 if [ -z "$2" ]; then
@@ -42,7 +52,6 @@ else
     echo "using env: "$2
     envName=$2
 fi
-
 
 
 prgPath=~/Data/work/$prgName
@@ -55,13 +64,16 @@ outputDir=/tmp/DevOutput
 outputPath=$outputDir/$prgName
 clionPath=~/Data/work/Clion
 
-scriptPath=/tmp/Script
+scriptPath="/tmp/Scripts/"$prgName"/"$envName
 scriptName=run_on_docker.sh
+
+echo "scriptPath:" $scriptPath
 
 [ -f $scriptPath/$scriptName ] || genRunScript
 
 
 echo "os:" $os
+echo "dts detected:" $optional_dts
 echo "projet path:" $prgPath
 echo "projet output:" $outputPath
 echo ""
@@ -88,5 +100,5 @@ docker run --rm -it                                                             
 -v $toolsPath:/tools                                                                                \
                                                                                                     \
 -v $outputPath:/prg/build/devel                                                                     \
-onixs-docker-images.jfrog.io/$os$dts/devel/ui/$envName                                                  \
+onixs-docker-images.jfrog.io/$os/devel/ui/$envName                                                  \
 /script/$scriptName
