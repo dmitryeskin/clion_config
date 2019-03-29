fd1e1112374420aa06fb32e0cc5d69fc8d09c895
Dmitry Eskin <dmitry.eskin@onixs.biz>
2019-03-29 21:08:10 +0300

    -

---
+++ projects/run_common.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+prgName=common-cpp
+
+echo "Opening "$prgName
+
+../run_project.sh $prgName
---
+++ projects/run_sgx_outch_ace_free.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+prgName=sgx-outch-cpp
+
+echo "Opening "$prgName
+
+../run_project.sh $prgName $prgName"-ace-free"
